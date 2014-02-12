81a5140d589f37f4d8c316eedaf42a5ed1f36bf5
Jubakuba <Jubakuba@gmail.com>
2014-02-12 03:13:42 -0700

    Settings: Notification Reminders (2/2)
    Change-Id: I6e65974c5dbfeac7a41dafe3ca127ac2b18ac1c9

--- src/com/android/settings/merk/interfacesettings/NotificationPanelSettings.java
+++ src/com/android/settings/merk/interfacesettings/NotificationPanelSettings.java
@@ -22,14 +22,20 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.preference.RingtonePreference;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
+import android.os.UserHandle;
 
 public class NotificationPanelSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -38,6 +44,16 @@ public class NotificationPanelSettings extends SettingsPreferenceFragment implem
     private static final String STATUS_BAR_CUSTOM_HEADER = "custom_status_bar_header";
 
     private CheckBoxPreference mStatusBarCustomHeader;
+
+    private static final String PREF_NOTI_REMINDER_SOUND =
+            "noti_reminder_sound";
+    private static final String PREF_NOTI_REMINDER_ENABLED =
+            "noti_reminder_enabled";
+    private static final String PREF_NOTI_REMINDER_RINGTONE =
+            "noti_reminder_ringtone";
+    private CheckBoxPreference mReminder;
+    private ListPreference mReminderMode;
+    private RingtonePreference mReminderRingtone;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,9 +67,39 @@ public class NotificationPanelSettings extends SettingsPreferenceFragment implem
         mStatusBarCustomHeader.setChecked(Settings.System.getInt(resolver,
             Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1);
         mStatusBarCustomHeader.setOnPreferenceChangeListener(this);
+
+        mReminder = (CheckBoxPreference) findPreference(PREF_NOTI_REMINDER_ENABLED);
+        mReminder.setChecked(Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.REMINDER_ALERT_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
+        mReminder.setOnPreferenceChangeListener(this);
+
+        mReminderMode = (ListPreference) findPreference(PREF_NOTI_REMINDER_SOUND);
+        int mode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.REMINDER_ALERT_NOTIFY, 0, UserHandle.USER_CURRENT);
+        mReminderMode.setValue(String.valueOf(mode));
+        mReminderMode.setOnPreferenceChangeListener(this);
+        updateReminderModeSummary(mode);
+
+        mReminderRingtone =
+                (RingtonePreference) findPreference(PREF_NOTI_REMINDER_RINGTONE);
+        Uri ringtone = null;
+        String ringtoneString = Settings.System.getStringForUser(getContentResolver(),
+                Settings.System.REMINDER_ALERT_RINGER, UserHandle.USER_CURRENT);
+        if (ringtoneString == null) {
+            // Value not set, defaults to Default Ringtone
+            ringtone = RingtoneManager.getDefaultUri(
+                    RingtoneManager.TYPE_RINGTONE);
+        } else {
+            ringtone = Uri.parse(ringtoneString);
+        }
+        Ringtone alert = RingtoneManager.getRingtone(getActivity(), ringtone);
+        mReminderRingtone.setSummary(alert.getTitle(getActivity()));
+        mReminderRingtone.setOnPreferenceChangeListener(this);
+        mReminderRingtone.setEnabled(mode != 0);
+
     }
 
-    @Override 
+    @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
 	return true;
     }
@@ -64,9 +110,46 @@ public class NotificationPanelSettings extends SettingsPreferenceFragment implem
             boolean value = (Boolean) objValue;
             Settings.System.putInt(resolver,
                 Settings.System.STATUS_BAR_CUSTOM_HEADER, value ? 1 : 0);
-        } else {
+        } else if (preference == mReminder) {
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.REMINDER_ALERT_ENABLED,
+                    (Boolean) objValue ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mReminderMode) {
+            int mode = Integer.valueOf((String) objValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.REMINDER_ALERT_NOTIFY,
+                    mode, UserHandle.USER_CURRENT);
+            updateReminderModeSummary(mode);
+            mReminderRingtone.setEnabled(mode != 0);
+            return true;
+        } else if (preference == mReminderRingtone) {
+            Uri val = Uri.parse((String) objValue);
+            Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), val);
+            mReminderRingtone.setSummary(ringtone.getTitle(getActivity()));
+            Settings.System.putStringForUser(getContentResolver(),
+                    Settings.System.REMINDER_ALERT_RINGER,
+                    val.toString(), UserHandle.USER_CURRENT);
+            return true;
+         } else {
             return false;
         }
         return true;
+    }
+
+    private void updateReminderModeSummary(int value) {
+        int resId;
+        switch (value) {
+            case 1:
+                resId = R.string.enabled;
+                break;
+            case 2:
+                resId = R.string.noti_reminder_sound_looping;
+                break;
+            default:
+                resId = R.string.disabled;
+                break;
+        }
+        mReminderMode.setSummary(getResources().getString(resId));
     }
 }
