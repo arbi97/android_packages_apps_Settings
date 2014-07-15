745224d6caa82c9b9fbc75279f9174bc5869157e
hashem78 <hashem.olayano@gmail.com>
2014-07-16 00:40:52 +0300

    Revert "This may fix buttons activty not working"

--- src/com/android/settings/merk/ButtonSettings.java
+++ src/com/android/settings/merk/ButtonSettings.java
@@ -30,7 +30,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.settings.merk;
+package com.android.settings;
 
 import java.util.prefs.PreferenceChangeListener;
 
