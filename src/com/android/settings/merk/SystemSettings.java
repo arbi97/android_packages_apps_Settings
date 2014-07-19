0e52f146fa6b92907db288da493b43a45abb9200
merkrehan <mrkrehanishaq1990@gmail.com>
2014-07-19 13:08:55 +0200

    remove omniswitch

--- src/com/android/settings/merk/SystemSettings.java
+++ src/com/android/settings/merk/SystemSettings.java
@@ -16,18 +16,13 @@
 
 package com.android.settings.merk;
 
-import android.content.Intent;
-import android.app.AlertDialog;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.RemoteException;
-import android.content.DialogInterface;
 import android.preference.CheckBoxPreference;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
@@ -51,25 +46,11 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     private static final String SHOW_RECENTS_MEMORY_INDICATOR = "show_recents_memory_indicator";
     private static final String RECENTS_MEMORY_INDICATOR_LOCATION =
             "recents_memory_indicator_location";
-    private static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
-    private static final String OMNISWITCH_START_SETTINGS = "omniswitch_start_settings";
-    private static final String CATEGORY_USE_OMNISWITCH = "category_omniswitch";
-
-    // Package name of the omnniswitch app
-    public static final String OMNISWITCH_PACKAGE_NAME = "org.omnirom.omniswitch";
-    // Intent for launching the omniswitch settings actvity
-    public static Intent INTENT_OMNISWITCH_SETTINGS = new Intent(Intent.ACTION_MAIN)
-            .setClassName(OMNISWITCH_PACKAGE_NAME, OMNISWITCH_PACKAGE_NAME + ".SettingsActivity");
 
     private CheckBoxPreference mRecentClearAll;
     private ListPreference mRecentClearAllPosition;
     private CheckBoxPreference mShowRecentsMemoryIndicator;
     private ListPreference mRecentsMemoryIndicatorPosition;
-    private CheckBoxPreference mRecentsUseOmniSwitch;
-    private Preference mOmniSwitchSettings;
-    private boolean mOmniSwitchInitCalled;
-    private PreferenceCategory mOmniSwitchCategory;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -103,33 +84,12 @@ public class SystemSettings extends SettingsPreferenceFragment implements
             mRecentsMemoryIndicatorPosition.setValue(recentsMemoryIndicatorPosition);
         }
         mRecentsMemoryIndicatorPosition.setOnPreferenceChangeListener(this);
-
-        mOmniSwitchCategory = (PreferenceCategory) prefSet.findPreference(CATEGORY_USE_OMNISWITCH);
-        boolean omniSwitchAvailable = isOmniSwitchAvailable();
-
-        if (!omniSwitchAvailable){
-            Settings.System.putInt(resolver, Settings.System.RECENTS_USE_OMNISWITCH, 0);
-            prefSet.removePreference(mOmniSwitchCategory);
-        } else {
-            mRecentsUseOmniSwitch = (CheckBoxPreference)
-                    prefSet.findPreference(RECENTS_USE_OMNISWITCH);
-            mRecentsUseOmniSwitch.setChecked(Settings.System.getInt(resolver,
-                    Settings.System.RECENTS_USE_OMNISWITCH, 0) == 1);
-            mRecentsUseOmniSwitch.setOnPreferenceChangeListener(this);
-
-            mOmniSwitchSettings = (Preference)
-                    prefSet.findPreference(OMNISWITCH_START_SETTINGS);
-            mOmniSwitchSettings.setEnabled(mRecentsUseOmniSwitch.isChecked());
         }
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mOmniSwitchSettings){
-            startActivity(INTENT_OMNISWITCH_SETTINGS);
             return true;
-        }
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
 
@@ -150,45 +110,11 @@ public class SystemSettings extends SettingsPreferenceFragment implements
             String value = (String) objValue;
             Settings.System.putString(
                     resolver, Settings.System.RECENTS_MEMORY_INDICATOR_LOCATION, value);
-        } else if (preference == mRecentsUseOmniSwitch) {
-            boolean value = (Boolean) objValue;
-
-            // if value has never been set before
-            if (value && !mOmniSwitchInitCalled){
-                openOmniSwitchFirstTimeWarning();
-                mOmniSwitchInitCalled = true;
-            }
-
-            Settings.System.putInt(
-                    resolver, Settings.System.RECENTS_USE_OMNISWITCH, value ? 1 : 0);
-            mOmniSwitchSettings.setEnabled(value);
         } else {
             return false;
         }
 
         return true;
-    }
-
-    private void openOmniSwitchFirstTimeWarning() {
-        new AlertDialog.Builder(getActivity())
-                .setTitle(getResources().getString(R.string.omniswitch_first_time_title))
-                .setMessage(getResources().getString(R.string.omniswitch_first_time_message))
-                .setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                        }
-                }).show();
-    }
-
-    private boolean isOmniSwitchAvailable() {
-        final PackageManager pm = getPackageManager();
-        try {
-            pm.getPackageInfo(OMNISWITCH_PACKAGE_NAME, PackageManager.GET_ACTIVITIES);
-            int enabled = pm.getApplicationEnabledSetting(OMNISWITCH_PACKAGE_NAME);
-            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
-                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
-        } catch (NameNotFoundException e) {
-            return false;
-        }
     }
      @Override
     public void onResume() {
